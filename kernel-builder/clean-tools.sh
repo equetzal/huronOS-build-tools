3451582c2af67658f7fce7f965b425df660bda5d
equetzal <eithnegomez@hotmail.com>
2022-03-15 20:56:30 -0600

    Fixed kernel pack and cleaning

--- kernel-builder/clean-tools.sh
+++ kernel-builder/clean-tools.sh
@@ -1,3 +1,6 @@
-apt remove --yes git git-core kernel-package fakeroot build-essential bc ncurses gcc make ld fdformat depmod e2fsck fsck.jfs reiserfsck xfs_db mksquashfs ersion btrfsck pccardctl quota pppd isdnctrl showmount ps oprofiled udevd grub mcelog iptables openssl version bc sphinx-build 
-apt autoremove --purge
-apt clean
+
+
+# Remove all installed tools used too compilation of kernel as they will not be used on final system
+apt remove --yes $PACKAGES
+apt autoremove --purge --yes
+apt clean --yes
--- kernel-builder/compile-kernel.sh
+++ kernel-builder/compile-kernel.sh
@@ -6,19 +6,20 @@
 pushd linux-$KERNEL_VERSION
 
 make clean
-#make -j $(getconf _NPROCESSORS_ONLN) deb-pkg LOCALVERSION=-huronos
-make -j $(getconf _NPROCESSORS_ONLN) bzImage LOCALVERSION=-huronos
-make -j $(getconf _NPROCESSORS_ONLN) modules LOCALVERSION=-huronos
+#make -j $(getconf _NPROCESSORS_ONLN) deb-pkg
+make -j $(getconf _NPROCESSORS_ONLN) bzImage
+make -j $(getconf _NPROCESSORS_ONLN) modules
 
 make modules_install INSTALL_MOD_STRIP=1
 
 TMP=/hos-ker
+NAME=$KERNEL_VERSION-huronos
 mkdir -p $TMP/usr/lib/modules
-cp -aR /usr/lib/modules/$KERNEL_VERSION $TMP/usr/lib/modules
-rm -f $TMP/usr/lib/modules/$KERNEL_VERSION/{build,source}
+cp -aR /usr/lib/modules/$NAME $TMP/usr/lib/modules
+rm -f $TMP/usr/lib/modules/$NAME/{build,source}
 mkdir -p $TMP/boot
-cp arch/x86/boot/bzImage $TMP/boot/vmlinuz-$KERNEL_VERSION
+cp arch/x86/boot/bzImage $TMP/boot/vmlinuz-$NAME
 cd $TMP
-tar -c * | gzip -f >/$KERNEL_VERSION-huronos.tar.gz
+tar -c * | gzip -f >/$NAME.tar.gz
 
 popd
