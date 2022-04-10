a68e3eb3876cbd50c55ce111fdc5c8dede319fa8
equetzal <eithnegomez@hotmail.com>
2022-04-10 14:29:03 -0500

    Extending firmware to improve hardware compatibility

--- software-modules/base/02-firmware/create.sh
+++ software-modules/base/02-firmware/create.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-FIRMWARE="firmware-linux-free firmware-atheros firmware-iwlwifi firmware-zd1211 firmware-realtek firmware-bnx2 firmware-brcm80211 firmware-cavium firmware-ipw2x00 firmware-libertas firmware-ti-connectivity firmware-b43-installer"
+# Author, the huronOS team:
+#	Enya Quetzalli <equetzal@huronos.org>
+
+FIRMWARE="arm-trusted-firmware-tools atmel-firmware bluez bluez-cups bluez-firmware bluez-obexd bluez-tools broadcom-sta-common broadcom-sta-dkms broadcom-sta-source dahdi-firmware-nonfree dns323-firmware-tools firmware-amd-graphics firmware-ath9k-htc firmware-atheros firmware-b43-installer firmware-bnx2 firmware-bnx2x firmware-brcm80211 firmware-cavium firmware-intel-sound firmware-intelwimax firmware-ipw2x00 firmware-ivtv firmware-iwlwifi firmware-libertas firmware-linux firmware-linux-free firmware-linux-nonfree firmware-microbit-micropython firmware-microbit-micropython-doc firmware-misc-nonfree firmware-myricom firmware-netronome firmware-netxen firmware-qcom-media firmware-qcom-soc firmware-qlogic firmware-realtek firmware-samsung firmware-siano firmware-sof-signed firmware-ti-connectivity firmware-tomu firmware-zd1211 hdmi2usb-fx2-firmware mesa-utils mesa-utils-extra mesa-va-drivers:amd64 mesa-vdpau-drivers:amd64 mesa-vulkan-drivers:amd64 midisport-firmware pulseaudio-module-gsettings pulseaudio-module-zeroconf sigrok-firmware-fx2lafw ubertooth-firmware ubertooth-firmware-source vulkan-tools vulkan-validationlayers:amd64"
 
 apt update
 apt install --yes --no-install-recommends $FIRMWARE
