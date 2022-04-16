d9c3ea0482df7fcf117a2bb61d045ef9c503ed46
equetzal <eithnegomez@hotmail.com>
2022-04-16 00:29:45 -0500

    Fixed GCC module

--- software-modules/langs/gcc/gcc.sh
+++ software-modules/langs/gcc/gcc.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
+set -xe
+
+## Install software
 apt update
 apt install --yes --no-install-recommends gcc
 apt autoremove --yes
 
-mkdir -p /usr/share/doc/reference
-cp -r ./reference/* /usr/share/doc/reference/
-
+## Prepare final files
+mkdir -p /usr/share/doc/reference/c/
+tar -xvzf reference.tar.gz -C /usr/share/doc/reference/c/ --strip-components=1
 cp ./c-documentation.desktop /usr/share/applications/
 
-savechanges /media/sdb1/huronOS/langs/gcc.hsm
+## Create packed changes
+savechanges /tmp/gcc.hsm
+
+## Clean package to maintain only relevant files
+hsm2dir /tmp/gcc.hsm
+rm -rf /tmp/gcc.hsm/var
+rm -rf /tmp/gcc.hsm/etc
+rm -rf /tmp/gcc.hsm/root
+rm -rf /tmp/gcc.hsm/home
+dir2hsm /tmp/gcc.hsm
+
+cp /tmp/gcc.hsm /run/initramfs/memory/data/huronOS/langs/
