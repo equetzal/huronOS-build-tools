5f5b1af98eacb0c8c1c5fde59c33c0ae305910d4
equetzal <eithnegomez@hotmail.com>
2022-04-16 16:03:32 -0500

    Fixed kate deactivation

--- software-modules/programming/kate/kate.sh
+++ software-modules/programming/kate/kate.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
 
+set -xe
+NAME=kate
+
+## Install software
 apt update
-apt install --yes --no-install-recommends kate
+apt install --yes --no-install-recommends $NAME
 apt autoremove --yes
 
-cp ./kate.desktop /usr/share/applications/
+## Prepare final files
+cp ./$NAME.desktop /usr/share/applications/
 
-savechanges ../../../../huronOS/programming/kate.hsm
+## Create packed changes
+savechanges /tmp/$NAME.hsm
+
+## Clean package to maintain only relevant files
+hsm2dir /tmp/$NAME.hsm
+rm -rf /tmp/$NAME.hsm/var
+rm -rf /tmp/$NAME.hsm/etc
+rm -rf /tmp/$NAME.hsm/root
+rm -rf /tmp/$NAME.hsm/home
+rm -rf /tmp/$NAME.hsm/usr/share/gnome
+rm -rf /tmp/$NAME.hsm/usr/share/icons/hicolor/icon-theme.cache
+rm -rf /tmp/$NAME.hsm/usr/share/applications/bamf-2.index
+rm -rf /tmp/$NAME.hsm/usr/share/applications/mimeinfo.cache
+rm -rf /tmp/$NAME.hsm/usr/share/applications/org.kde.kate.desktop
+dir2hsm /tmp/$NAME.hsm
+
+cp /tmp/$NAME.hsm /run/initramfs/memory/data/huronOS/programming/
