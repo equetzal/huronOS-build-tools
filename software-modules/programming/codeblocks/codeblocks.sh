36edda95394d10412f1b1644f64f1f83ad906314
equetzal <eithnegomez@hotmail.com>
2022-04-16 14:11:28 -0500

    Fixed codeblocks deactivation

--- software-modules/programming/codeblocks/codeblocks.sh
+++ software-modules/programming/codeblocks/codeblocks.sh
@@ -1,10 +1,31 @@
 #!/bin/sh
 
+set -xe
+
+## Install software
 apt update
 apt install --yes --no-install-recommends codeblocks
 apt autoremove --yes
 
+## Prepare final files
 cp ./codeblocks.desktop /usr/share/applications/
 
-savechanges ../../../../huronOS/programming/codeblocks.hsm
+## Create packed changes
+savechanges /tmp/codeblocks.hsm
 
+## Clean package to maintain only relevant files
+hsm2dir /tmp/codeblocks.hsm
+rm -rf /tmp/codeblocks.hsm/var
+rm -rf /tmp/codeblocks.hsm/etc
+rm -rf /tmp/codeblocks.hsm/root
+rm -rf /tmp/codeblocks.hsm/home
+rm -rf /tmp/codeblocks.hsm/usr/share/mime
+rm -rf /tmp/codeblocks.hsm/usr/share/gnome
+rm -rf /tmp/codeblocks.hsm/usr/share/metainfo
+rm -rf /tmp/codeblocks.hsm/usr/share/lintian
+rm -rf /tmp/codeblocks.hsm/usr/share/icons
+rm -rf /tmp/codeblocks.hsm/usr/share/applications/bamf-2.index
+rm -rf /tmp/codeblocks.hsm/usr/share/applications/mimeinfo.cache
+dir2hsm /tmp/codeblocks.hsm
+
+cp /tmp/codeblocks.hsm /run/initramfs/memory/data/huronOS/programming/
