a871c89282697fe1e3dd9f114a14fdd92fb7b686
equetzal <eithnegomez@hotmail.com>
2022-04-16 00:33:09 -0500

    Fixed Ruby module

--- software-modules/langs/ruby/ruby.sh
+++ software-modules/langs/ruby/ruby.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
-mkdir -p /usr/share/doc/ruby-documentation
-cp -r ./documentation/* /usr/share/doc/ruby-documentation/
+set -xe
 
+## Install software
+apt update
+apt install --yes --no-install-recommends ruby
+
+## Prepare final files
+mkdir -p /usr/share/doc/reference/ruby/
+tar -xvzf reference.tar.gz --strip-components=1 -C /usr/share/doc/reference/ruby/
 cp ./ruby-documentation.desktop /usr/share/applications/
 
-savechanges /media/sdb1/huronOS/langs/ruby.hsm
+## Create packed changes
+savechanges /tmp/ruby.hsm
+
+## Clean package to maintain only relevant files
+hsm2dir /tmp/ruby.hsm
+rm -rf /tmp/ruby.hsm/var
+rm -rf /tmp/ruby.hsm/home
+rm -rf /tmp/ruby.hsm/root
+rm -rf /tmp/ruby.hsm/etc
+dir2hsm /tmp/ruby.hsm
+
+cp /tmp/ruby.hsm /run/initramfs/memory/data/huronOS/langs/
