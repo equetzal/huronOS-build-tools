7a02796ba1eacfeb8dbcbd144b2fd860df24f7ba
equetzal <eithnegomez@hotmail.com>
2022-04-16 00:31:05 -0500

    Fixed Python3 module

--- software-modules/langs/python3/python3.sh
+++ software-modules/langs/python3/python3.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
-mkdir -p /usr/share/doc/python3-documentation/
-cp -r ./documentation/* /usr/share/doc/python3-documentation/
+set -xe
 
+## Install software
+# Python3 is special case, it is dependency for budgie, so we will need to manage it with permissions.
+
+## Prepare final files
+mkdir -p /usr/share/doc/reference/python3/
+tar -xvzf reference.tar.gz --strip-components=1 -C /usr/share/doc/reference/python3/
 cp ./python3-documentation.desktop /usr/share/applications/
 
-savechanges /media/sdb1/huronOS/langs/python3.hsm
+## Create packed changes
+savechanges /tmp/python3.hsm
+
+## Clean package to maintain only relevant files
+hsm2dir /tmp/python3.hsm
+rm -rf /tmp/python3.hsm/var
+rm -rf /tmp/python3.hsm/home
+rm -rf /tmp/python3.hsm/root
+rm -rf /tmp/python3.hsm/etc
+dir2hsm /tmp/python3.hsm
+
+cp /tmp/python3.hsm /run/initramfs/memory/data/huronOS/langs/
