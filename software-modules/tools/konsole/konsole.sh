ce86ac7344ecb9419ac7a7f381c93bcfd0b114a8
equetzal <eithnegomez@hotmail.com>
2022-04-16 22:10:40 -0500

    Fixing Konsole deactivation

--- software-modules/tools/konsole/konsole.sh
+++ software-modules/tools/konsole/konsole.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
+set -xe
+NAME=konsole
+
+## Install software
 apt update
-apt install --yes --no-install-recommends konsole
+apt install --yes --no-install-recommends $NAME
 apt autoremove --yes
 
-cp ./konsole.desktop /usr/share/applications/
-rm org.kde.konsole.desktop
+## Prepare final files
+cp ./$NAME.desktop /usr/share/applications/
 
-savechanges ../../../../huronOS/tools/konsole.hsm
+## Create packed changes
+savechanges /tmp/$NAME.hsm
+
+## Clean package to maintain only relevant files
+hsm2dir /tmp/$NAME.hsm
+rm -rf /tmp/$NAME.hsm/var
+rm -rf /tmp/$NAME.hsm/root
+rm -rf /tmp/$NAME.hsm/home
+rm -rf /tmp/$NAME.hsm/etc
+rm -rf /tmp/$NAME.hsm/usr/share/gnome
+rm -rf /tmp/$NAME.hsm/usr/share/applications/bamf-2.index
+rm -rf /tmp/$NAME.hsm/usr/share/applications/mimeinfo.cache
+rm -rf /tmp/$NAME.hsm/usr/share/applications/org.kde.konsole.desktop
+dir2hsm /tmp/$NAME.hsm
+
+cp /tmp/$NAME.hsm /run/initramfs/memory/data/huronOS/tools/
+echo "Finished creating $NAME.hsm!"
