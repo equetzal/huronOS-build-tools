772adaec24fa8cda677f5235e7513732053a12b8
equetzal <eithnegomez@hotmail.com>
2022-04-26 21:29:32 -0500

    Adding vim

---
+++ software-modules/programming/vim/vim.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -xe
+NAME=vim
+
+## Install software
+apt update
+apt install --yes --no-install-recommends $NAME
+apt autoremove --yes
+
+## Prepare final files
+cp ./$NAME.desktop /usr/share/applications/
+
+## Create packed changes
+savechanges /tmp/$NAME.hsm
+
+## Clean package to maintain only relevant files
+hsm2dir /tmp/$NAME.hsm
+rm -rf /tmp/$NAME.hsm/var
+rm -rf /tmp/$NAME.hsm/root
+rm -rf /tmp/$NAME.hsm/home
+rm -rf /tmp/$NAME.hsm/usr/share/gnome
+rm -rf /tmp/$NAME.hsm/usr/share/mime
+rm -rf /tmp/$NAME.hsm/usr/share/icons/hicolor/icon-theme.cache
+rm -rf /tmp/$NAME.hsm/usr/share/applications/bamf-2.index
+rm -rf /tmp/$NAME.hsm/usr/share/applications/mimeinfo.cache
+dir2hsm /tmp/$NAME.hsm
+
+cp /tmp/$NAME.hsm /run/initramfs/memory/data/huronOS/programming/
+echo "Finished creating $NAME.hsm!"
