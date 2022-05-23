6d67187d2da44783c9939ef0659c8f308f3a1cc6
Enya Quetzalli <equetzal@huronos.org>
2022-05-23 00:54:59 -0500

    Adding persistence to hsync, replacing old hsync

--- base-system/usrroot/usr/lib/hsync/hsync.sh
+++ base-system/usrroot/usr/lib/hsync/hsync.sh
@@ -16,6 +16,7 @@ readonly BASE_MNT=$MEMORY/base
 readonly MODULES_MNT=$MEMORY/modules
 readonly UNION=/
 readonly DIRECTIVES_FILE=/etc/hsync/directives
+readonly DIRECTIVES_DEFAULT=/etc/hsync/default
 readonly DIRECTIVES_FILE_SERVER=/etc/hsync/server
 readonly STATE_FILE=/etc/hsync/state
 readonly BACKUP_DIR_NAME=.huronOS..sysbackup.d
@@ -24,8 +25,11 @@ readonly BACKUP_DIR=$USRCHANGES/$BACKUP_DIR_NAME
 
 ## Set some global variables
 
-	# STATE preffix is the current state that is being applied.
-	# they're set along the process and the writted to disk
+	declare EXECUTION_IS_SCHEDULED_APPLY
+	declare EXECUTION_IS_ROUTINE
+	declare EXECUTION_NEXT_SCHEDULED_APPLY_TIME
+
+	# STATE preffix is the current state that the system have.
 	declare STATE_IS_CLOCK_SYNC
 	declare STATE_MODE
 	declare STATE_MODE_START_TIME_UTC
@@ -34,26 +38,25 @@ readonly BACKUP_DIR=$USRCHANGES/$BACKUP_DIR_NAME
 	declare STATE_PERSISTENCE_DISK
 	declare STATE_LAST_HSYNC_EXECUTION_TIME_UTC
 
-	# DISK_STATE preffix is the last state that was writted to disk
-	# it is used to change the system state to a newer one.
-	declare DISK_STATE_IS_CLOCK_SYNC
-	declare DISK_STATE_MODE
-	declare DISK_STATE_MODE_START_TIME_UTC
-	declare DIKS_STATE_MODE_END_TIME_UTC
-	declare DISK_STATE_IS_PERSISTENCE_ENABLED
-	declare DISK_STATE_PERSISTENCE_DISK
-	declare DISK_STATE_LAST_HSYNC_EXECUTION_TIME_UTC
+	# NEW preffix is for intermediate variables to set the new
+	# state.
+	declare NEW_MODE
+	declare NEW_MODE_START_TIME_UTC
+	declare NEW_MODE_END_TIME_UTC
+	declare NEW_PERSISTENCE_DISK
 
 	# DIRECTIVES preffix is the information about the directives file
 	declare DIRECTIVES_FILE_URL
 	declare DIRECTIVES_TEMP_FILE
-	declare DIRECTIVES_HAVE_CHANGED
-	declare DIRECTIVES_GLOBAL_CONFIG_HAVE_CHANGED
-	declare DIRECTIVES_ALWAYS_CONFIG_HAVE_CHANGED
-	declare DIRECTIVES_EVENT_CONFIG_HAVE_CHANGED
-	declare DIRECTIVES_CONTEST_CONFIG_HAVE_CHANGED
-	declare DIRECTIVES_EVENT_TIMES_HAVE_CHANGED
-	declare DIRECTIVES_CONTEST_TIMES_HAVE_CHANGED
+	declare DIRECTIVES_FILE_TO_USE
+	declare DIRECTIVES_SPECIFIC_CONFIG
+	declare DIRECTIVES_HAVE_CHANGED=1
+	declare DIRECTIVES_GLOBAL_CONFIG_HAVE_CHANGED=1
+	declare DIRECTIVES_ALWAYS_CONFIG_HAVE_CHANGED=1
+	declare DIRECTIVES_EVENT_CONFIG_HAVE_CHANGED=1
+	declare DIRECTIVES_CONTEST_CONFIG_HAVE_CHANGED=1
+	declare DIRECTIVES_EVENT_TIMES_HAVE_CHANGED=1
+	declare DIRECTIVES_CONTEST_TIMES_HAVE_CHANGED=1
 
 
 ## Include libraries of hsync
@@ -66,6 +69,9 @@ readonly BACKUP_DIR=$USRCHANGES/$BACKUP_DIR_NAME
 
 
 main(){
+	log_start
+	check_execution_arguments "$@"
+
 	# Hsync is executed every 1 minute to mantain the system updated acording to the directives file
 	# but, the first execution after boot is crucial to know if the system needs to restore it state
 	# before boot, or if it will take a new configuration. If no persistence was set on boot, then we
@@ -108,6 +114,8 @@ main(){
 	if try_directives_download && have_directives_changed; then
 		update_directives
 		apply_directives_to_system
+	elif is_execution_scheduled_apply; then
+		apply_directives_to_system
 	fi
 
 	# Save the state of the system on the state file so that we can handle correctly future
@@ -121,6 +129,23 @@ main(){
 	if system_has_persistece_enabled; then
 		backup_state_to_disk
 	fi
+	log_end
+}
+
+is_execution_scheduled_apply(){
+	if [ "$EXECUTION_IS_SCHEDULED_APPLY" = "true" ]; then
+		return 0 # true
+	fi
+	return 1 # false
+}
+
+check_execution_arguments(){
+	case $1 in
+		"--help") exit ;;
+		"--scheduled-apply") EXECUTION_IS_SCHEDULED_APPLY="true";;
+		"--routine-sync") EXECUTION_IS_ROUTINE="true";;
+		*) exit;;
+	esac
 }
 
 main "$@"; exit;
