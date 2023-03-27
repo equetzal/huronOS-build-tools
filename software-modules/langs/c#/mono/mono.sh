a0590585359a39869bc3823bc7c42a16430a145f
equetzal <equetzal@huronos.org>
2023-03-26 21:52:41 -0600

    [Sofware] Adding C# Mono compiler

---
+++ software-modules/langs/c#/mono/mono.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#	mono.sh
+#	Script to build the modular software package of Mono
+#	.NET Framework open source implementation.
+#	It purges the unnecessary files on the FS to allow AUFS
+#	add/del operations on the fly.
+#
+#	Copyright (C) 2022, huronOS Project:
+#		<http://huronos.org>
+#
+#	Licensed under the GNU GPL Version 2
+#		<http://www.gnu.org/licenses/gpl-2.0.html>
+#
+#	Authors:
+#		Enya Quetzalli <equetzal@huronos.org>
+
+set -xe
+NAME=mono
+TARGET="/run/initramfs/memory/system/huronOS/software/langs/"
+
+## Add mono repo keyring
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/debian stable-buster main" | tee /etc/apt/sources.list.d/mono-official-stable.list
+
+## Install software
+apt update
+apt install --yes --no-install-recommends mono-devel
+apt autoremove --yes
+
+## Create packed changes
+savechanges /tmp/$NAME.hsm
+
+## Clean package to maintain only relevant files
+hsm2dir /tmp/$NAME.hsm
+rm -rf /tmp/$NAME.hsm/var
+rm -rf /tmp/$NAME.hsm/root
+rm -rf /tmp/$NAME.hsm/home
+rm -rf /tmp/$NAME.hsm/etc
+rm -rf /tmp/$NAME.hsm/etc/apt
+rm -rf /tmp/$NAME.hsm/etc/ssl
+rm -rf /tmp/$NAME.hsm/etc/ld.so.cache
+rm -rf /tmp/$NAME.hsm/etc/ca-certificates
+rm -rf /tmp/$NAME.hsm/usr/sbin
+rm -rf /tmp/$NAME.hsm/usr/share/gnome
+rm -rf /tmp/$NAME.hsm/usr/share/mime
+rm -rf /tmp/$NAME.hsm/usr/share/icons/hicolor/icon-theme.cache
+rm -rf /tmp/$NAME.hsm/usr/share/applications/bamf-2.index
+rm -rf /tmp/$NAME.hsm/usr/share/applications/mimeinfo.cache
+find /tmp/$NAME.hsm/
+dir2hsm /tmp/$NAME.hsm
+
+mkdir -p "$TARGET"
+cp /tmp/$NAME.hsm "$TARGET"
+echo "Finished creating $NAME.hsm!"
