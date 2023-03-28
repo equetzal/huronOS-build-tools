8fc9cc27ab5a4c93b40144796a9ff545b78957c7
equetzal <equetzal@huronos.org>
2023-03-27 21:17:53 -0600

    [Software] Updating crow translate to fix icon issues

--- software-modules/internet/crow/crow.sh
+++ software-modules/internet/crow/crow.sh
@@ -9,17 +9,18 @@
 #		<http://huronos.org>
 #
 #	Licensed under the GNU GPL Version 2
-#		<http://www.gnu.org/licenses/gpl-2.0.html>	
+#		<http://www.gnu.org/licenses/gpl-2.0.html>
 #
 #	Authors:
 #		Enya Quetzalli <equetzal@huronos.org>
 
 set -xe
 NAME=crow
+TARGET="/run/initramfs/memory/system/huronOS/software/internet/"
 
 ## Install software
 apt update
-apt install --yes --no-install-recommends ./crow-translate_2.8.4-focal1_amd64.deb
+apt install --yes --no-install-recommends "./crow-translate_2.10.3_amd64.deb"
 apt autoremove --yes
 
 ## Prepare final files
@@ -39,7 +40,8 @@ rm -rf /tmp/$NAME.hsm/usr/share/icons/hicolor/icon-theme.cache
 rm -rf /tmp/$NAME.hsm/usr/share/applications/bamf-2.index
 rm -rf /tmp/$NAME.hsm/usr/share/applications/mimeinfo.cache
 rm -rf /tmp/$NAME.hsm/usr/share/applications/io.crow*
+find /tmp/$NAME.hsm
 dir2hsm /tmp/$NAME.hsm
 
-cp /tmp/$NAME.hsm /run/initramfs/memory/system/huronOS/internet/
+cp /tmp/$NAME.hsm "$TARGET"
 echo "Finished creating $NAME.hsm!"
