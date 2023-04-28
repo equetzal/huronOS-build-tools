fca862b5b39f80536f5880f158d5585eccf8b8c6
DT3264 <cernadaniel32@gmail.com>
2023-04-27 22:39:29 -0600

    [#23] Allows wallpapers with no hash file

--- base-system/usrroot/usr/lib/hos/utils.sh
+++ base-system/usrroot/usr/lib/hos/utils.sh
@@ -16,15 +16,16 @@
 ## asuming that all parameters are provided and URLs are valid
 # $1 = URL of image
 # $2 = File path to save
-# $3 = URL of sha256
-safe_download(){
-	local FILE_URL FILE_HASH_URL SAVE_PATH TMP TMP_FILE
+# $3 = Sha256 of wallpaper
+safe_download() {
+	local FILE_URL SAVE_PATH FILE_HASH FILE_NAME TMP TMP_FILE TMP_HASH_FILE
 	FILE_URL="$1"
 	SAVE_PATH="$2"
-	FILE_HASH_URL="$3"
+	FILE_HASH="$3"
+	FILE_NAME=$(basename "$FILE_URL")
 	TMP="/tmp/safe-download-$$"
-	TMP_FILE="$TMP/$(basename "$FILE_URL")"
-	TMP_HASH_FILE="$TMP/$(basename "$FILE_HASH_URL")"
+	TMP_FILE="$TMP/$FILE_NAME"
+	TMP_HASH_FILE="$TMP/${FILE_NAME}.sha256"
 
 	## Try the download into temp
 	mkdir -p "$TMP"
@@ -34,24 +35,18 @@ safe_download(){
 		return 1 # Error
 	fi
 
-	if [ ! -z "$FILE_HASH_URL" ]; then
-		## If hash cannot be downloaded, then it's not possible to verify file
-		if ! wget --no-cache --tries=3 --timeout=10 "$FILE_HASH_URL" -O "$TMP_HASH_FILE"; then
-			echo "Cannot download the hash file $FILE_HASH_URL" 1>&2
-			rm -rf "$TMP"
-			return 1 # Error
-		fi
-
-		## If hash does not match, do not save file
-		pushd "$TMP"
-		if ! sha256sum --status --check "$TMP_HASH_FILE"; then
-			echo "Downloaded file does not match it's hash, aborting. $FILE_URL" 1>&2
-			popd
-			rm -rf "$TMP"
-			return 1 # Error
-		fi
+	pushd "$TMP"
+	## Write hash into hash file
+	echo "$FILE_HASH  $FILE_NAME" >"$TMP_HASH_FILE"
+	## If hash does not match, do not save file
+	pushd "$TMP"
+	if ! sha256sum --status --check "$TMP_HASH_FILE"; then
+		echo "Downloaded file does not match it's hash, aborting. $FILE_URL" 1>&2
 		popd
+		rm -rf "$TMP"
+		return 1 # Error
 	fi
+	popd
 
 	## Everything seems to be fine, copy file to final path
 	cp -f "$TMP_FILE" "$SAVE_PATH"
@@ -60,16 +55,20 @@ safe_download(){
 	return 0 # Success
 }
 
-
-## Given an URL and it's hash, returns if it should redownload a file or not
+## Given a file and it's hash, returns if it should redownload a file or not
 # $1 = Saved file path
-# $2 = URL of sha256
-should_redownload_file(){
-	local FILE_PATH FILE_HASH_URL TMP TMP_FILE TMP_HASH_FILE
+# $2 = URL of wallpaper
+# $3 = Sha256 of wallpaper
+should_redownload_file() {
+	local FILE_PATH FILE_URL FILE_HASH FILE_NAME TMP TMP_FILE TMP_HASH_FILE
 	FILE_PATH="$1"
-	FILE_HASH_URL="$2"
+	FILE_URL="$2"
+	FILE_HASH="$3"
+	FILE_NAME=$(basename "$FILE_URL")
+	## Get the basename of the wallpaper as mentioned in the hash
 	TMP="/tmp/verify-download-$$"
-	TMP_HASH_FILE="$TMP/$(basename "$FILE_HASH_URL")"
+	TMP_FILE="$TMP/$FILE_NAME"
+	TMP_HASH_FILE="$TMP/${FILE_NAME}.sha256"
 
 	## If file does not exists, then yes, we need to redownload it.
 	if [ ! -f "$FILE_PATH" ]; then
@@ -77,18 +76,12 @@ should_redownload_file(){
 	fi
 
 	mkdir -p "$TMP"
-	## Local file exists, but we cannot download its hash. Assume current file is not valid
-	if ! wget --no-cache --tries=3 --timeout=10 "$FILE_HASH_URL" -O "$TMP_HASH_FILE"; then
-		echo "Cannot download the hash file $FILE_HASH_URL" 1>&2
-		rm -rf "$TMP"
-		return 0 # Yes, download
-	fi
-
 	## Copy local file to match hash file name
-	TMP_FILE="$TMP/$(cat "$TMP_HASH_FILE" | awk '{ print $2 }' )"
 	cp -f "$FILE_PATH" "$TMP_FILE"
 
 	pushd "$TMP"
+	## Write hash into hash file
+	echo "$FILE_HASH  $FILE_NAME" >"$TMP_HASH_FILE"
 	## If the local file does not match the hash, we need to redownload the file
 	if ! sha256sum --status --check "$TMP_HASH_FILE"; then
 		echo "Local file does not match remote hash, file is not the same." 1>&2
