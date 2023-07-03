851a2b989b8064883be7eb97f3d13d87edfe5437
DT3264 <cernadaniel32@gmail.com>
2023-07-03 19:20:06 -0400

    Adds bookmarks sync in browsers (through an extension)

--- software-modules/internet/chromium/chromium.sh
+++ software-modules/internet/chromium/chromium.sh
@@ -20,11 +20,31 @@ TARGET_DIR="/run/initramfs/memory/system/huronOS/software/internet/"
 
 ## Install software
 apt update
-apt install --yes --no-install-recommends $NAME
+apt install --yes --no-install-recommends $NAME python-is-python3
 apt autoremove --yes
 
 ## Prepare final files
 cp ./$NAME.desktop /usr/share/applications/
+
+## Prepare bookmarks extension
+# The extension
+curl -o /usr/share/bookmark-bridge.crx -L https://github.com/huronOS/bookmark-bridge/releases/latest/download/bookmark-bridge.crx
+
+# The extension loader
+CHROMIUM_EXT_LOADER_PATH="/usr/share/chromium/extensions"
+mkdir -p $CHROMIUM_EXT_LOADER_PATH
+curl -o "$CHROMIUM_EXT_LOADER_PATH/cbfajpicdmpnnoijdmhdomaailmdaiim.json" https://raw.githubusercontent.com/huronOS/bookmark-bridge/development/chrome-extension-loader.json
+
+# The extension's native host
+CHROMIUM_NATIVE_HOST="/etc/chromium/native-messaging-hosts"
+mkdir -p $CHROMIUM_NATIVE_HOST
+curl -o "$CHROMIUM_NATIVE_HOST/bookmarks_fetcher.json" https://raw.githubusercontent.com/huronOS/bookmark-bridge/development/chrome-native-host.json
+
+# The middleman between huron and the browser
+curl -o /usr/bin/bookmarks-fetcher.py https://raw.githubusercontent.com/huronOS/bookmark-bridge/development/app/bookmarks-fetcher.py
+curl -o /usr/bin/current-bookmarks https://raw.githubusercontent.com/huronOS/bookmark-bridge/development/app/current-bookmarks
+chmod +x /usr/bin/bookmarks-fetcher.py
+chmod +x /usr/bin/current-bookmarks
 
 ## Create packed changes
 savechanges /tmp/$NAME.hsm
