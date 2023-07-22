63c920cb47fd7c917aa991bc430420325deb632e
DT3264 <cernadaniel32@gmail.com>
2023-07-22 15:50:16 -0600

    Moves hos-wallpaper to the packages folder
    While moving hos-wallpaper to the packages folder, it also adds a script to copy the usrroot files and every package that might be needed to copy (such as hos-wallpaper), such script will also allow to easily copy all of those files to an usb drive so it would be easier to both copy and test such changes

---
+++ base-system/tools/rootcopy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#	rootcopy.sh
+#	Copies the usrroot files
+#   and other folders with a rootlike structure
+#   to the specified USB drive
+#
+#	Copyright (C) 2022, huronOS Project:
+#		<http://huronos.org>
+#
+#	Licensed under the GNU GPL Version 2
+#		<http://www.gnu.org/licenses/gpl-2.0.html>
+#
+#	Authors:
+#		Daniel Cerna <cernadaniel@huronos.org>
+
+# Goes to the root of the project
+cd ../../
+# Obtains the target path
+TARGET=$1
+shift 1
+if [ -z "$1" ]; then
+    echo "No USB path specified, please run [rootcopy.sh <USB_PATH> [--is-usb]]"
+    exit 1
+fi
+# Allows to specify if the target is a USB drive
+if [ -n "$1" ] && [ "$1" = "--is-usb" ]; then
+    # If target is a USB drive, then we go to the rootcopy folder
+    TARGET="$TARGET/huronOS/rootcopy"
+    mkdir -p "$TARGET"
+fi
+cp -r base-system/usrroot/* "$TARGET"
+cp -r packages/hos-wallpaper/* "$TARGET"
