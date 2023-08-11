d2fbc00f68ff55188bc5ab075ba277c258cee882
DT3264 <cernadaniel32@gmail.com>
2023-08-11 17:56:02 -0600

    [vscode-extensions] Refactoring extensions
    # How to test
    - pull this PR
    - open vscodium and in the extensions tab type @builtin, keep track of the extension count on each section
    - close vscodium
    - execute ./make-extensions.sh (after that all of the 5 modules should be under /run/initramfs...)
    - enable all of the 5 extensions
    - open vscodium and look again for the builtin extensions and now you should have 5 more :D

---
+++ software-modules/programming/vscode-extensions/vsc-cpp-compile-run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#	vsc-cpp-compile-run.sh
+#	Script to build the modular software package of Visual Studio Code
+#	cpp-compile-run extension.
+#
+#	Copyright (C) 2023, huronOS Project:
+#		<http://huronos.org>
+#
+#	Licensed under the GNU GPL Version 2
+#		<http://www.gnu.org/licenses/gpl-2.0.html>
+#
+#	Authors:
+#		Daniel Cerna <dcerna@huronos.org>
+
+set -xe
+
+EXTENSION_NAME="vsc-cpp-compile-run"
+EXTENSION_FILE="$EXTENSION_NAME.vsix"
+EXTENSION_URL="https://github.com/danielpinto8zz6/c-cpp-compile-run/releases/download/v1.0.45/c-cpp-compile-run-1.0.45.vsix"
+EXTENSION_FOLDER="$EXTENSION_NAME/extension"
+HSM_FILE="$EXTENSION_NAME.hsm"
+# Downloads extension
+curl -o "$EXTENSION_FILE" -L "$EXTENSION_URL"
+# Extracts current extension
+mkdir "$EXTENSION_NAME"
+unzip -q "$EXTENSION_FILE" -d "$EXTENSION_NAME"
+# Prepares lab folder
+LAB_FOLDER="$EXTENSION_NAME-hsm-lab/"
+DESTINATION_FOLDER="$LAB_FOLDER/usr/share/codium/resources/app/extensions/"
+mkdir -p "$DESTINATION_FOLDER"
+# Moves extension to the destination folder
+mv "$EXTENSION_FOLDER" "$DESTINATION_FOLDER/$EXTENSION_NAME"
+# Creates the huronOS module
+mksquashfs "$LAB_FOLDER" "$HSM_FILE" -quiet
+# Removes any temporary file
+rm -r "$LAB_FOLDER" "$EXTENSION_FILE" "$EXTENSION_NAME"
+# Moves the module to the final destination
+mv "$HSM_FILE" /run/initramfs/memory/system/huronOS/software/programming/
