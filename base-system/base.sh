405941c479f858b465fdcaab2a7b2f237a4620db
equetzal <equetzal@huronos.org>
2024-02-06 20:18:52 -0800

    Creating sysforge: A tool for automatically building the huronOS system layers

--- base-system/base.sh
+++ base-system/base.sh
@@ -21,6 +21,7 @@
 
 set -xe
 
+# shellcheck source-path=./config
 . "./config" || exit 1
 
 CHANGEDIR="$(dirname "$(readlink -f "$0")")"
@@ -68,10 +69,10 @@ mkdir -p "$EFI"
 mkdir -p "${ISO_DATA}/utils"
 mkdir -p "$FILES"/base
 mkdir -p "$FILES"/data
-mkdir -p "$FILES"/data/logs
-mkdir -p "$FILES"/data/journal
 mkdir -p "$FILES"/data/backups
 mkdir -p "$FILES"/data/configs
+mkdir -p "$FILES"/data/journal
+mkdir -p "$FILES"/data/logs
 mkdir -p "$FILES"/software
 mkdir -p "$FILES"/software/debuggers
 mkdir -p "$FILES"/software/internet
