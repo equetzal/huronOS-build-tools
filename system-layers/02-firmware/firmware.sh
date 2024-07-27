f2dd91006eafe83a783b3ee4d27a4fda1d33b0ff
Daniel Cerna <cernadaniel32@gmail.com>
2024-07-27 11:40:54 -0600

    [debian12] Adding nvidia driver
    This PR adds the required things in order to include the nvidia-driver with the smallest footprint possible

--- system-layers/02-firmware/firmware.sh
+++ system-layers/02-firmware/firmware.sh
@@ -33,5 +33,9 @@ export DEBIAN_FRONTEND=noninteractive
 apt install --yes --no-install-recommends "${DEPENDENCIES[@]}"
 apt autoremove --yes --purge
 
+## Install the nvidia drivers
+cd nvidia
+./install-nvidia-drivers.sh
+
 savechanges /tmp/02-firmware.hsl
 cp /tmp/02-firmware.hsl /run/initramfs/memory/system/huronOS/base --verbose
